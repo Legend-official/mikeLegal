e6418b2950ef31a70e2582756ba6f96426a7a48a
Simranjit <simranjitsingh4661@gmail.com>
2024-11-20 22:22:49 +0530

    be changes added

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -1,2 +1,5 @@
-const API_KEY = import.meta.env.VITE_MOVIES_API_KEY;
-export const HOST = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+export const API_KEY = import.meta.env.VITE_MOVIES_API_KEY;
+// export const HOST = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+export const HOST = `https://mikelegalworker.simranjitsingh019.workers.dev/api/`;
+export const MOVIES = "movies";
+export const MOVIES_DETAILS = "movie-details";
--- src/api/getApi.ts
+++ src/api/getApi.ts
@@ -1,11 +1,13 @@
 import { APIClient } from "./axios";
+import { MOVIES, MOVIES_DETAILS, API_KEY } from "./endpoint";
 
-// fetch movies by search string and api is paginated 
+// fetch movies by search string and api is paginated
 export const fetchMovies = async (searchTerm: string, page: number) => {
-  const response = await APIClient().get("", {
+  const response = await APIClient().get(MOVIES, {
     params: {
-      s: searchTerm,
       page: page,
+      s: searchTerm,
+      apikey: API_KEY,
     },
   });
 
@@ -18,9 +20,10 @@ export const fetchMovies = async (searchTerm: string, page: number) => {
 
 // fetch movies detail based on the movie ID
 export const fetchMoviesDetail = async (imdbID: string) => {
-  const response = await APIClient().get("", {
+  const response = await APIClient().get(MOVIES_DETAILS, {
     params: {
       i: imdbID,
+      apikey: API_KEY,
     },
   });
 
